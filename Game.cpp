8b69a9d8f8582696367c37e51310132259e9a0e3
Osama <os14you.manage@gmail.com>
2025-07-31 15:19:21 +0300

    fixing some structure and code clearity

--- Game.cpp
+++ Game.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "Header/Tic-Tac-Toe.hpp"
+
 int menu(const bool &isFirstTime){
     int choice;
     if(isFirstTime)
-        std::cout<<"---------- Welcome To Our Game ----------\n\n";
-    std::cout<<"1. Single player Mode \n2. Multiplayer Mode\n3. Exit\n";
-    std::cout<<"Enter your choice : ";
-    std::cin>>choice;
-    while(choice<1 || choice >3){
+        std::cout << "---------- Welcome To Our Game ----------\n\n";
+    std::cout << "1. Single player Mode \n2. Multiplayer Mode\n3. Exit\n";
+    std::cout << "Enter your choice : ";
+    std::cin >> choice;
+
+    while(choice < 1 || choice > 3){
         std::cout<<"Enter a valid input : ";
         std::cin>>choice;
     }
@@ -15,11 +17,12 @@ int menu(const bool &isFirstTime){
 }
 
 void aGame(){
-    bool forOne = true;
+    bool isFirstTime = true;
     while(true){
         Game game;
-        int choice = menu(forOne);
-        forOne = false;
+        int choice = menu(isFirstTime);
+        isFirstTime = false;
+        
         switch (choice){
             case 1: game.singlePlayer(); break;
             case 2: game.multiPlayer(); break;
