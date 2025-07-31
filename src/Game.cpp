1463b75261f8f04b8fbf2f09d4b5d95fd0fa722c
Osama <os14you.manage@gmail.com>
2025-07-31 20:32:33 +0300

    Refactoring the Game header and implementation

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,95 +1,61 @@
 #include <Game.hpp>
 
-Game::Game() : Game('X') {}
-Game::Game(char turn) : turn(turn), turns(0){
-    std::cout<<"Game Started !!\n\n";
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++)
-            board[i][j] = 0;
-    }
+Game::Game(char player1_type, char player2_type) {
+    // Create Player 1 ('X') based on the specified type
+    if (player1_type == 'h')
+        player1 = std::make_unique<HumanPlayer>('X');
+    else
+        player1 = std::make_unique<AIPlayer>('X');
+
+    // Create Player 2 ('O') based on the specified type
+    if (player2_type == 'h')
+        player2 = std::make_unique<HumanPlayer>('O');
+    else
+        player2 = std::make_unique<AIPlayer>('O');
+
+    // Set the starting player
+    currentPlayer = player1.get();
 }
 
-void Game::winner(char player){
-    std::cout<<"\n\nWinnnnnnnnnerrrrr !! PLAYER "<<player<<" is the WINNER !!!!\n\n";
+void Game::run() {
+    // The game loop continues as long as the game is not over
+    while (!is_game_over()) {
+        board.display();
+
+        int row, col;
+        currentPlayer->make_move(board, row, col); // Ask the current player for their move
+
+        board.place_move(row, col, currentPlayer->get_symbol()); 
+        switch_player();
+    }
+
+    // Display the final board state once the game has ended
+    board.display();
 }
 
-void Game::printBoard(){
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            if(!board[i][j]) std::cout<<" - ";
-            else if(board[i][j]==1) std::cout<<" X ";
-            else std::cout<<" O ";
-        }
-        std::cout<<std::endl;
-    }
+void Game::switch_player() {
+    currentPlayer = (currentPlayer == player1.get()) ? player2.get() : player1.get();
 }
 
-int Game::playTurn(int x,int y){
-    if(x<0 || x>2 || y<0 || y>2 || board[x][y]){
-        std::cout<<"\n\nInvalid input . Try again\n\n"<<std::endl;
-        return 0;
+bool Game::is_game_over() {
+    // Check if player 1 has won
+    if (board.check_win(player1->get_symbol())) {
+        std::cout << ">>> Player " << player1->get_symbol() << " wins!\n";
+        return true;
     }
-    else if(this->turn == 'X') board[x][y]=1;
-    else board[x][y]=2;
-    return 1;
-}
 
-bool Game::checkBoard(){
-    for(int check=0;check<8;check++){
-        int r = rowNum[check] , c = colNum[check] , ic_r = increaseRow[check] , ic_c = increaseCol[check];
-        int cnt = 0 , first = board[r][c];
-        if(first==0)
-            continue;
-        for(int steps=0;steps<3; steps++ , r+=ic_r , c+=ic_c)
-            cnt += (first == board[r][c]);
-        if(cnt == 3){
-            winner(turn);
-            return true;
-        }
+    // Check if player 2 has won
+    if (board.check_win(player2->get_symbol())) {
+        std::cout << ">>> Player " << player2->get_symbol() << " wins!\n";
+        return true;
     }
-    this->turn = (this->turn == 'X' ? 'O':'X');
+
+    // Check if the game is a draw
+    if (board.is_full()) {
+        std::cout << ">>> The game is a draw!\n";
+        return true;
+    }
+
+    // If none of the above, the game is not over
     return false;
-}
-
-void Game::multiPlayer(){
-    while(true){
-        if(turns == 9){
-            std::cout<<"\n\nTie Game!\n\n"; break;
-        }
-        std::cout<<"Player ( "<<this->turn<<" ) Turn"<<std::endl;
-        std::cout<<"Enter your choice [row,column]: ";
-        int row,column;
-        std::cin>>row>>column; 
-        row--,column--;
-        if(!playTurn(row,column))
-            continue;
-        printBoard();
-        if(checkBoard())
-            break;
-        turns++;
-    }
-}
-
-void Game::singlePlayer(){
-    while(true){
-        std::cout<<"Player ( X ) Turn"<<std::endl;
-        std::cout<<"Enter your choice [row,column]: ";
-        int row,column;
-        std::cin>>row>>column; 
-        row--,column--;
-        if(!playTurn(row,column))
-            continue;
-        printBoard();
-        if(checkBoard())
-            break;
-        turns++;
-        if(turns == 9){
-            std::cout<<"\n\nTie Game!\n\n"; break;
-        }
-        agent.getMove(this->board,false);
-        printBoard();
-        if(checkBoard())
-            break;
-        turns++;
-    }
 }
